8e14f114af80e3ec1d3bc7f55403a8f224d17031
mamoru9045 <mamoru@sandbox-vr.com>
2022-10-11 08:26:12 +0900

    first commit

--- backend/settings.py
+++ backend/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR=os.path.join(BASE_DIR,'google_app/templates/')
-print(TEMPLATE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -32,13 +31,13 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'google_app', # 追加 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'google_app', # 追加 
     'rest_framework', # 追加
     'webpack_loader', #追加
     'corsheaders',#追加
--- google_app/models.py
+++ google_app/models.py
@@ -1,6 +1,8 @@
+from calendar import calendar
 from distutils.command.upload import upload
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+# from django_mysql.models import ListCharField
 
 # Create your models here.
 class User(models.Model):
@@ -10,6 +12,8 @@ class User(models.Model):
     old = models.IntegerField(default = 0)
     sex = models.CharField(default = "mamoru",max_length=10)
     data = JSONField(default = {"name":"mamoru"})
+    # calendar_list = ListCharField(models.CharField(max_length=100),size=6, max_length=(6 * 11))
+
 
     class Meta:
        app_label = 'google_app'
