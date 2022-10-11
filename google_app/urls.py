1a407953efafdff9051e50822192403c0a6abb1f
mamoru9045 <mamoru@sandbox-vr.com>
2022-10-11 11:59:09 +0900

    db作成

--- google_app/urls.py
+++ google_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 from google_app.views import IndexView
 
+
+app_name = 'google_app'
 urlpatterns = [
     path('',IndexView.as_view(),name='index'),
     ]
